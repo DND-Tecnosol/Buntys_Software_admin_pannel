4eb36fb137079fd2c153f88c86ecf051e32278d3
bhautik2304 <rathodbhautikrameshbhai@gmail.com>
2023-03-03 18:24:31 +0530

    old changes

--- src/Pages/Setting/index.js
+++ src/Pages/Setting/index.js
@@ -1,44 +1,44 @@
 // Business Settings
-export {default as Storemanager} from './BusnessSettings/Storemanager'
-export {default as StoreClosedate} from './BusnessSettings/StoreClosedate'
-export {default as ManageStoreTime} from './BusnessSettings/ManageStoreTime'
-export {default as StoreNotyfication} from './BusnessSettings/StoreNotyfication'
-export {default as Expense} from './BusnessSettings/Expense'
-export {default as Security} from './BusnessSettings/Security'
-export {default as StaffAttendence} from './BusnessSettings/StaffAttendence'
+export { default as Storemanager } from './BusnessSettings/Storemanager'
+export { default as StoreClosedate } from './BusnessSettings/StoreClosedate'
+export { default as ManageStoreTime } from './BusnessSettings/ManageStoreTime'
+export { default as StoreNotyfication } from './BusnessSettings/StoreNotyfication'
+export { default as Expense } from './BusnessSettings/Expense'
+export { default as Security } from './BusnessSettings/Security'
+export { default as StaffAttendence } from './BusnessSettings/StaffAttendence'
 
 // Data & migration Settings
-export {default as UplodeCostumerData} from './Data&migration/UplodeCostumerData'
-export {default as UplodeMasterData} from './Data&migration/UplodeMasterData'
+export { default as UplodeCostumerData } from './Data&migration/UplodeCostumerData'
+export { default as UplodeMasterData } from './Data&migration/UplodeMasterData'
 
 
 
 // Module Level Settings
-export {default as SericeManager} from './ModuleSettings/SericeManager'
-export {default as ProductManager} from './ModuleSettings/productManager'
-export {default as StaffManager} from './ModuleSettings/StaffManager'
-export {default as UserManger} from './ModuleSettings/UserManger'
-export {default as Invoicemanager} from './ModuleSettings/Invoicemanager'
-export {default as Vaoucher} from './ModuleSettings/Vaoucher'
-export {default as RewardPointManager} from './ModuleSettings/RewardPointManager'
-export {default as Offers} from './ModuleSettings/Offers'
-export {default as MembershipeManager} from './ModuleSettings/MembershipeManager'
-export {default as PakgeManager} from './ModuleSettings/PakgeManager'
-export {default as CostumerManager} from './ModuleSettings/CostumerManager'
+export { default as SericeManager } from './ModuleSettings/SericeManager'
+export { default as ProductManager } from './ModuleSettings/productManager'
+export { default as StaffManager } from './ModuleSettings/StaffManager'
+export { default as UserManger } from './ModuleSettings/UserManger'
+export { default as Invoicemanager } from './ModuleSettings/invoicemanager/'
+export { default as Vaoucher } from './ModuleSettings/Vaoucher'
+export { default as RewardPointManager } from './ModuleSettings/RewardPointManager'
+export { default as Offers } from './ModuleSettings/Offers'
+export { default as MembershipeManager } from './ModuleSettings/MembershipeManager'
+export { default as PakgeManager } from './ModuleSettings/PakgeManager'
+export { default as CostumerManager } from './ModuleSettings/CostumerManager'
 
 
 // Store Service
-export {default as Inventory} from './StoreService/Inventory'
-export {default as VenderManager} from './StoreService/VenderManager'
-export {default as CaseRegister} from './StoreService/CaseRegister'
-export {default as OnlineAppointment} from './StoreService/OnlineAppointment'
+export { default as Inventory } from './StoreService/Inventory'
+export { default as VenderManager } from './StoreService/VenderManager'
+export { default as CaseRegister } from './StoreService/CaseRegister'
+export { default as OnlineAppointment } from './StoreService/OnlineAppointment'
 
 
 // Store Service
-export {default as WebsiteManager} from './Website/WebsiteManager'
+export { default as WebsiteManager } from './Website/WebsiteManager'
 
 
 // 3rd Party Integrate Service
-export {default as SmsServices} from './IntigrationSetting/SmsServices'
-export {default as WhatsappServices} from './IntigrationSetting/WhatsappServices'
-export {default as DailySoftwareTask} from './IntigrationSetting/DailySoftwareTask'
+export { default as SmsServices } from './IntigrationSetting/SmsServices'
+export { default as WhatsappServices } from './IntigrationSetting/WhatsappServices'
+export { default as DailySoftwareTask } from './IntigrationSetting/DailySoftwareTask'
